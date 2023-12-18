2573f03677bc1487e4cd12ea97df9fedf04509b1
HSYN <huseyn.mirzayev@code.edu.az>
2023-12-18 21:40:21 +0400

    add data and assets

---
+++ data.js
@@ -0,0 +1,38 @@
+const songs = [
+  {
+    name: "glimpse-of-us",
+    artist: "Joji",
+    title: "Glimpse of Us",
+    duration: "3:53",
+  },
+  {
+    name: "fourth-of-july",
+    artist: "Sufjan Stevens",
+    title: "Fourth of July",
+    duration: "4:38",
+  },
+  {
+    name: "indigo-night",
+    artist: "Tamino",
+    title: "Indigo Night",
+    duration: "4:14",
+  },
+  {
+    name: "remembrance",
+    artist: "Balmorhea",
+    title: "Remembrance",
+    duration: "5:59",
+  },
+  {
+    name: "summertime-sadness",
+    artist: "Lana del Rey",
+    title: "Summertime Sadness",
+    duration: "3:25",
+  },
+  {
+    name: "i-know-i-am-not-the-only-one",
+    artist: "Sam Smith",
+    title: "I Know I'm Not The Only One",
+    duration: "3:57",
+  },
+];
